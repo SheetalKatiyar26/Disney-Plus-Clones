4f61aea128443d488d355981e28af2403f3eab2f
Sheetal <105560345+SheetalKatiyar26@users.noreply.github.com>
2022-08-03 16:32:15 +0530

    Updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBdWO9zKaQ2UjLW6XVmy69I9pP1rPJS-Y",
-  authDomain: "disney-plus-clone-23ce8.firebaseapp.com",
-  projectId: "disney-plus-clone-23ce8",
-  storageBucket: "disney-plus-clone-23ce8.appspot.com",
-  messagingSenderId: "715618917639",
-  appId: "1:715618917639:web:032fb49095310b53cbf9a2",
-  measurementId: "G-55TMYQ3TKT",
+  apiKey: "AIzaSyDHFoA-Q3gG_Dz6qvo-S_1-Y0V6jILvdzc",
+  authDomain: "disney-clone-47555.firebaseapp.com",
+  databaseURL: "https://disney-clone-47555-default-rtdb.firebaseio.com",
+  projectId: "disney-clone-47555",
+  storageBucket: "disney-clone-47555.appspot.com",
+  messagingSenderId: "651592295923",
+  appId: "1:651592295923:web:b9c432d28a3e774f029e86",
 };
 
+// Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
