eb1691e8a283eff971e6ec4af83a28975e921549
Sheetal <105560345+sheetal2604@users.noreply.github.com>
2022-11-01 14:11:14 +0530

    Updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,17 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAYV75X4Mb59lpBlukgjK8M05G4rD6buvI",
-  authDomain: "disneyplus-89406.firebaseapp.com",
-  databaseURL: "https://disneyplus-89406-default-rtdb.firebaseio.com",
-  projectId: "disneyplus-89406",
-  storageBucket: "disneyplus-89406.appspot.com",
-  messagingSenderId: "635389230996",
-  appId: "1:635389230996:web:014454b738a2cc4b559981"
+  apiKey: "AIzaSyCUYddo6MS9ksN9FcT-Sm4aIFAlxvCutPk",
+  authDomain: "disney-d2036.firebaseapp.com",
+  databaseURL: "https://disney-d2036-default-rtdb.firebaseio.com",
+  projectId: "disney-d2036",
+  storageBucket: "disney-d2036.appspot.com",
+  messagingSenderId: "115964213881",
+  appId: "1:115964213881:web:97637f2ccc5796af2c32a8"
 };
+
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
